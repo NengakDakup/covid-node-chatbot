88fb84f8fba2991a9f1488041ffc5aa01f2d715d
nengak <nengakdakup@gmail.com>
2020-04-04 12:28:13 +0100

    changed the route to post

--- routes/api/incoming.js
+++ routes/api/incoming.js
@@ -1,105 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const dotenv = require('dotenv');
-const twilio = require('twilio');
-const axios = require('axios');
+const dotenv = require("dotenv");
+const twilio = require("twilio");
+const axios = require("axios");
 
 dotenv.config();
-
 
 // @route   GET api/auth/test
 // @desc    Gets a message from a user
 // @access  public
-router.post('/', (req, res) => {
-    const client = twilio(process.env.twilio_accountSid, process.env.twilio_authToken); 
+router.post("/", (req, res) => {
+  const client = twilio(
+    process.env.twilio_accountSid,
+    process.env.twilio_authToken
+  );
 
-    const { MessagingResponse } = twilio.twiml;
+  const { MessagingResponse } = twilio.twiml;
 
-    let welcome = ``;
-  const msg = [];
-
-    async function fetchData(){
-      await axios.get('https://pomber.github.io/covid19/timeseries.json')
-    .then(response => {
-      const {data} = response;
-      for (var country in data) {
-        if (data.hasOwnProperty(country)) {
-          const {confirmed, deaths, recovered} = data[country][0];
-          const put = {
-            country,
-            ...data[country][0]
-          };
-
-          msg.push(put);
-
-        }
-      }
-
-      welcome = `
-    *Welcome to the Way Nerd COVID-19 Helper* %0a
-    %0a
-    Get Information and guidance from WHO regarding the current outbreak of coronavirus %0a
-    %0a
-    For Now we are still working on the bot, but you can view this information for now %0a
-    %0a
-    *Countries stats about corona virus*
-    %0a
-    ${msg.map(record => {
-      return `
-        *${record.country}*%0a
-        *confirmed*: ${record.confirmed}%0a
-        *deaths*: ${record.deaths}%0a
-        *recovered*: ${record.recovered}%0a
-        %0a
-      `
-    })}
-
-    `;
-
-    client.messages 
-        .create({ 
-          body: `${welcome}` , 
-          from: 'whatsapp:+14155238886',       
-          to: 'whatsapp:+2347057325184' 
-        }) 
-        .then(message => console.log(message.sid)) 
-        .done();
-    
-      })
-    };
-
-    fetchData();
-
-    
-});
-
-
-
-router.get('/test', (req, res) => {
-  console.log('loading');
   let welcome = ``;
   const msg = [];
 
-    async function fetchData(){
-      await axios.get('https://pomber.github.io/covid19/timeseries.json')
-    .then(response => {
-      const {data} = response;
-      for (var country in data) {
-        if (data.hasOwnProperty(country)) {
-          const {confirmed, deaths, recovered} = data[country][0];
-          const put = {
-            country,
-            ...data[country][0]
-          };
+  async function fetchData() {
+    await axios
+      .get("https://pomber.github.io/covid19/timeseries.json")
+      .then(response => {
+        const { data } = response;
+        for (var country in data) {
+          if (data.hasOwnProperty(country)) {
+            const put = {
+              country,
+              ...data[country][0]
+            };
 
-          msg.push(put);
-
+            msg.push(put);
+          }
         }
-      }
 
-      welcome = `
-    *Welcome to the Way Nerd COVID-19 Helper* %0a
+    welcome = `
+    *Welcome to the WavyNerd COVID-19 Helper* %0a
     %0a
     Get Information and guidance from WHO regarding the current outbreak of coronavirus %0a
     %0a
@@ -114,19 +53,27 @@ router.get('/test', (req, res) => {
         *deaths*: ${record.deaths}%0a
         *recovered*: ${record.recovered}%0a
         %0a
-      `
+      `;
     })}
 
     `;
 
-    res.json(welcome);
-    })
-    };
+        client.messages
+          .create({
+            body: `${welcome}`,
+            from: "whatsapp:+14155238886",
+            to: "whatsapp:+2347057325184"
+          })
+          .then(message => console.log(message.sid))
+          .done();
+      });
+  }
 
-    fetchData();
-  
-})
+  fetchData();
+});
 
-
+router.get("/test", (req, res) => {
+  console.log("loading");
+});
 
 module.exports = router;
